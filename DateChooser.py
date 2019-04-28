759718b64f19c2b36b555c877705dc8103fb1318
JWoodbr <39112647+JWoodbr@users.noreply.github.com>
2019-04-28 22:50:50 +1000

    Update DateChooser.py

--- DateChooser.py
+++ DateChooser.py
@@ -6,13 +6,33 @@ Created on Tue Apr 16 14:28:33 2019
 """
 import random
 import csv
+import wget
+import os.path
+
+def Download_file():
+    link_to_data = "https://raw.githubusercontent.com/JWoodbr/JustTheLittleThings/master/DateIdeas.csv"
+    wget.download(link_to_data)
+
+
+
+if os.path.isfile("DateIdeas.csv"):
+    os.remove('DateIdeas.csv')
+    Download_file()
+else:
+    Download_file()
+
+
 
 file = open("DateIdeas.csv")
 numline = len(file.readlines())
+file.close()
 
 with open('DateIdeas.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
-    rows = list(csv_reader)
+    rows = list(csv_file)
     
 dateIdea = random.randint(1, (numline-1))
 print((rows[dateIdea]))
+
+
+#sort date ideas by cheap, summer, spring,expensive, etc
